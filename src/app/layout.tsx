05c77c79b73014475fcc80894cd5a7aed83dc385
extasys66-mod <extasys66@proton.me>
2026-01-19 11:18:25 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import Script from "next/script"
 import { AuthProvider } from "@/hooks/use-auth"
+import MetaPixel from "@/components/MetaPixel"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,39 +19,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <head>
-        {/* Facebook Pixel */}
-        <Script
-          id="facebook-pixel"
-          strategy="afterInteractive"
-        >
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-
-            fbq('init', '25202599599419905');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-      </head>
-
       <body className={inter.className}>
-        {/* Facebook Pixel noscript */}
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=25202599599419905&ev=PageView&noscript=1"
-          />
-        </noscript>
-
+        <MetaPixel />
         <AuthProvider>
           {children}
         </AuthProvider>
