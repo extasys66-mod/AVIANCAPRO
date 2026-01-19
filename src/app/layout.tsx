8b81c33ca53361cdc197899aae8e575d7263d6fc
extasys66-mod <extasys66@proton.me>
2026-01-19 11:05:51 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,9 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { AuthProvider } from "@/hooks/use-auth"
-import Script from 'next/script' // Importamos el componente Script
+import Script from "next/script"
 
-const inter = Inter({ subsets: ["latin"], })
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Avianca - Encuentra tiquetes y vuelos baratos",
@@ -14,35 +14,41 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang="es">
       <head>
-        {/* Google Tag Manager - Script principal */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
+        {/* 🔵 META PIXEL */}
+        <Script id="meta-pixel" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5MG3JX56');
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '25202599599419905');
+            fbq('track', 'PageView');
           `}
         </Script>
       </head>
+
       <body className="font-display">
-        {/* Google Tag Manager (noscript) - Fallback para cuando JS está desactivado */}
+        {/* 🔵 Fallback sin JavaScript */}
         <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5MG3JX56"
-            height="0" 
-            width="0" 
-            style={{ display: 'none', visibility: 'hidden' }}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=25202599599419905&ev=PageView&noscript=1"
           />
         </noscript>
 
-        <AuthProvider> 
+        <AuthProvider>
           {children}
         </AuthProvider>
       </body>
